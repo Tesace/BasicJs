100423c38b57aa493c39821d5a4d235c1ae96426
Tesace <35670480+Tesace@users.noreply.github.com>
2022-06-22 16:56:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = ("My name is Oghenetega Okukulabe.");
+document.getElementById("height").innerHTML = ("I am 180cm tall.");
+document.getElementById("country").innerHTML = ("I am from Nigeria.");
